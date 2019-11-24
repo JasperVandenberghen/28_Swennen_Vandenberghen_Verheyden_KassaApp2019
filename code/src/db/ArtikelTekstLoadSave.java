1c53ed1fc14e670f313322f8b8f631281bae65e7
Jasper <jasper.vandenberghen@gmail.com>
2019-11-24 22:54:49 +0100

    excel plugins toevoegd, klasses aangemaakt

--- code/src/db/ArtikelTekstLoadSave.java
+++ code/src/db/ArtikelTekstLoadSave.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class ArtikelTekstLoadSave extends TekstLoadSaveTemplate {
+public class ArtikelTekstLoadSave extends TekstLoadSaveTemplate implements LoadSave{
 
     @Override
     public List load(){
---
+++ code/src/db/LoadSave.java
@@ -0,0 +1,10 @@
+package db;
+
+import java.util.List;
+
+public interface LoadSave {
+
+    List load();
+    void save(List list);
+
+}
