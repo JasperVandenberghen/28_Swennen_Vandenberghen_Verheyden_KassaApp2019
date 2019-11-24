1a5fa4a28c960d7459956e7377a5a321f6be705e
Jasper <jasper.vandenberghen@gmail.com>
2019-11-24 23:20:59 +0100

    main update

--- code/src/application/Main.java
+++ code/src/application/Main.java
@@ -1,11 +1,16 @@
 package application;
 	
+import db.ArtikelDb;
+import db.ArtikelTekstLoadSave;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import view.KassaView;
 import view.KlantView;
 
+import java.util.ArrayList;
+
 public class Main extends Application {
+
 	@Override
 	public void start(Stage primaryStage) {
 		KassaView kassaView = new KassaView();
@@ -13,6 +18,12 @@ public class Main extends Application {
 	}
 	
 	public static void main(String[] args) {
+		ArtikelDb artikelDb = new ArtikelDb();
+		ArtikelTekstLoadSave artikelTekstLoadSave = new ArtikelTekstLoadSave();
+		ArrayList artikelen = (ArrayList) artikelTekstLoadSave.load();
+		artikelDb.addMultiple(artikelen);
+		System.out.println(artikelDb);
+
 		launch(args);
 	}
 }
