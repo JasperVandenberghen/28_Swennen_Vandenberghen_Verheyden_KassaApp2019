0ae64c1c4667b295cdb5377ac70a9f47aa4cdfb3
Jasper <jasper.vandenberghen@gmail.com>
2019-12-05 12:49:07 +0100

    Revert "Revert "ArtikelExcelLoadSaveStrategy edit, excelplugin jar op juiste manier nu""
    This reverts commit 7733adff1d8a10b158036e4a9e02b04e81e5b6c8.

--- code/src/model/db/LoadSaveStrategy.java
+++ code/src/model/db/LoadSaveStrategy.java
@@ -1,16 +1,15 @@
 package model.db;
 
 import jxl.read.biff.BiffException;
+import jxl.write.WriteException;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public interface LoadSaveStrategy {
 
     List<?> load() throws IOException, BiffException;
-    void save(List list);
+    void save(String file, List list) throws WriteException, IOException, BiffException;
     String getFile();
 
 }
