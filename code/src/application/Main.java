3950d59b916c74b3370072fda29227316246f9de
Jeroen Verheyden <jeroen.verheyden2@student.ucll.be>
2019-12-05 18:23:47 +0100

    Test

--- code/src/application/Main.java
+++ code/src/application/Main.java
@@ -42,7 +42,7 @@ public class Main extends Application {
 
 
 		KlantView klantView = new KlantView(kassaKlantController);
-
+		System.out.println("hll");
 	}
 	
 	public static void main(String[] args) {
