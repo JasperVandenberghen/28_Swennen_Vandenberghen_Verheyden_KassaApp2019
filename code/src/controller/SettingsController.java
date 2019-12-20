60f9fd0bed7843a7f2cc77985a5ce4ba846e53c0
Jasper <jasper.vandenberghen@gmail.com>
2019-12-20 14:41:55 +0100

    properties voor headers & footers slaan nu op

--- code/src/controller/SettingsController.java
+++ code/src/controller/SettingsController.java
@@ -23,7 +23,7 @@ public class SettingsController {
         return kassaSettingsPane;
     }
 
-    public void setProperties(String dbType, String typeLoadSave, String typeKorting, String aantalKorting, String categorie, String drempel ){
+    public void setProperties(String dbType, String typeLoadSave, String typeKorting, String aantalKorting, String categorie, String drempel, String algemeneHeader, String datumTijdHeader, String prijsKortingFooter, String prijsBtwFooter, String algemeneFooter ){
         Properties properties = new Properties();
         properties.setProperty("dbType", dbType);
         properties.setProperty("typeLoadSave", typeLoadSave);
@@ -31,6 +31,11 @@ public class SettingsController {
         properties.setProperty("aantalKorting", aantalKorting);
         properties.setProperty("drempelKorting", drempel);
         properties.setProperty("categorieKorting", categorie);
+        properties.setProperty("algemeneHeader", algemeneHeader);
+        properties.setProperty("datumTijdHeader", datumTijdHeader);
+        properties.setProperty("prijsKortingFooter", prijsKortingFooter);
+        properties.setProperty("prijsBtwFooter", prijsBtwFooter);
+        properties.setProperty("algemeneFooter", algemeneFooter);
         this.propertiesHandler.write(properties);
     }
     public void addKorting(String kortingStr, String hoeveelheid, String categorie, String drempel){
--- code/src/view/panels/KassaSettingsPane.java
+++ code/src/view/panels/KassaSettingsPane.java
@@ -4,7 +4,10 @@ import controller.SettingsController;
 import javafx.collections.FXCollections;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
-import model.domain.*;
+import model.domain.ArtikelDbEnum;
+import model.domain.KortingEnum;
+import model.domain.MessageHandler;
+import model.domain.SaveEnum;
 
 import java.util.List;
 
@@ -18,7 +21,11 @@ public class KassaSettingsPane extends VBox {
     private Label kortingsAantalLabel;
     private Label drempelLabel;
     private Label categorieLabel;
-    private Button voegKortingToe;
+    private CheckBox algemeneBoodschapHeader;
+    private CheckBox datumTijdHeader;
+    private CheckBox totalePrijsKortingFooter;
+    private CheckBox prijsBtwFooter;
+    private CheckBox algemeneFooter;
     private TextField kortingsAantalField;
     private TextField drempelAantal;
     private TextField categorie;
@@ -26,6 +33,12 @@ public class KassaSettingsPane extends VBox {
     private List<String> dbTypeList = ArtikelDbEnum.getSaveStrategies();
     private List<String> kortingTypeList = KortingEnum.getKortingen();
     private Button opslaan;
+    private String algemeneHeader;
+    private String datumtijdHeader;
+    private String prijsKortingFooter;
+    private String prijsbtwFooter;
+    private String algemeneboodschapFooter;
+
 
     public KassaSettingsPane(SettingsController settingsController) {
 //        this.setPadding(new Insets(5,5,5, 5));
@@ -39,6 +52,11 @@ public class KassaSettingsPane extends VBox {
         drempelLabel = new Label("Voeg hier je drempel toe indien drempelkorting.");
         categorieLabel = new Label("Voeg hier je categorie toe indien groepskorting.");
         kortingsAantalField = new TextField();
+        algemeneBoodschapHeader = new CheckBox("Algemene boodschap toevoegen aan header");
+        datumTijdHeader = new CheckBox("Datum & tijd toevoegen aan header");
+        totalePrijsKortingFooter = new CheckBox("Voeg totale prijs + korting toe aan footer");
+        prijsBtwFooter = new CheckBox("Voeg prijs met btw toe aan footer");
+        algemeneFooter = new CheckBox("Voeg een algemene boodschap toe aan footer");
         drempelAantal = new TextField();
         categorie = new TextField();
         kortingsAantalField.setTranslateX(5);
@@ -51,9 +69,8 @@ public class KassaSettingsPane extends VBox {
         loadSaveBox.getSelectionModel().selectFirst();
         dbTypeBox.getSelectionModel().selectFirst();
         kortingTypeBox.getSelectionModel().selectFirst();
-        voegKortingToe = new Button("Voeg korting toe");
         opslaan = new Button("Opslaan");
-        this.getChildren().addAll( dbTypeLabel, dbTypeBox, loadSaveLabel,loadSaveBox, kortingTypeLabel, kortingTypeBox, drempelLabel, drempelAantal, categorieLabel, categorie, kortingsAantalLabel, kortingsAantalField, opslaan);
+        this.getChildren().addAll( dbTypeLabel, dbTypeBox, loadSaveLabel,loadSaveBox, kortingTypeLabel, kortingTypeBox, drempelLabel, drempelAantal, categorieLabel, categorie, kortingsAantalLabel, kortingsAantalField, algemeneBoodschapHeader, datumTijdHeader, totalePrijsKortingFooter, prijsBtwFooter, algemeneFooter, opslaan);
 
 
 
@@ -76,12 +93,19 @@ public class KassaSettingsPane extends VBox {
             alert.showAndWait();
         });*/
 
+
+
         opslaan.setOnAction(event -> {
+            if(algemeneBoodschapHeader.isSelected()){algemeneHeader = "true";} else {algemeneHeader = "false";}
+            if(datumTijdHeader.isSelected()){datumtijdHeader = "true";} else {datumtijdHeader = "false";}
+            if(totalePrijsKortingFooter.isSelected()){prijsKortingFooter = "true";} else {prijsKortingFooter = "false";}
+            if(prijsBtwFooter.isSelected()){prijsbtwFooter = "true";} else {prijsbtwFooter = "false";}
+            if(algemeneFooter.isSelected()){algemeneboodschapFooter = "true";} else {algemeneboodschapFooter = "false";}
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("Korting toevoegen");
             alert.setHeaderText(null);
             if(!kortingsAantalField.getText().trim().isEmpty()){
-                if(Double.parseDouble(kortingsAantalField.getText()) > 0 && (Double.parseDouble(kortingsAantalField.getText()) <= 100)){
+                if(Double.parseDouble(kortingsAantalField.getText()) >= 0 && (Double.parseDouble(kortingsAantalField.getText()) <= 100)){
                     settingsController.addKorting(kortingTypeBox.getValue().toString(), kortingsAantalField.getText(), categorie.getText(), drempelAantal.getText());
                     alert.setContentText("Korting succesvol toegevoegd");}
                 else{
@@ -93,7 +117,8 @@ public class KassaSettingsPane extends VBox {
             }
 
             alert.showAndWait();
-            settingsController.setProperties(dbTypeBox.getValue().toString(), loadSaveBox.getValue().toString(), kortingTypeBox.getValue().toString(),kortingsAantalField.getText(), categorie.getText(), drempelAantal.getText());
+            settingsController.setProperties(dbTypeBox.getValue().toString(), loadSaveBox.getValue().toString(), kortingTypeBox.getValue().toString(),kortingsAantalField.getText(), categorie.getText(), drempelAantal.getText(), algemeneHeader, datumtijdHeader, prijsKortingFooter, prijsbtwFooter, algemeneboodschapFooter);
+
             MessageHandler.showAlert("Successfully saved");
         });
 
